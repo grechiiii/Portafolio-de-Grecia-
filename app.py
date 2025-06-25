b2821b3ac4b9163b8e2e9ccb79e2c4d08c5eedf3
grechiiii <a20234861@pucp.edu.pe>
2025-06-25 02:59:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ Siempre busco aprender, colaborar y crear impacto desde la comunicación.""",
    "Tableau": "",
    "Medium": "https://www.linkedin.com/in/grecia-garcia-hoyos-44997933a/",
    "City": "Lima, Perú",
-   "Photo":"""<a href="https://www.linkedin.com/in/grecia-garcia-hoyos-44997933a/"><img src="https://imgur.com/FT8NVWT" width="200" alt="Grecia García Hoyos" title="Grecia García Hoyos"></a>""",
+   "Photo":"""<a href="https://www.linkedin.com/in/grecia-garcia-hoyos-44997933a/"><img src="https://imgur.com/Noy3lNI" width="200" alt="Grecia García Hoyos" title="Grecia García Hoyos"></a>""",
    "Email": "a20234861@pucp.edu.pe"
 }
 
